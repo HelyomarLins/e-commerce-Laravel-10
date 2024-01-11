2324787053cc97a9d8ef101fe1509e75fbe04e33
Helyomar Lins <111030700+HelyomarLins@users.noreply.github.com>
2024-01-10 22:41:34 -0300

    Incluindo linck modal jquary
    Inserindo div com modal

--- resources/views/compra/historico.blade.php
+++ resources/views/compra/historico.blade.php
@@ -21,14 +21,31 @@
                 {{ $ped->statusDesc() }}
             </td>
             <td>
-                <a href="#" class="btn btn-sm btn-info">
+                <a href="#" class="btn btn-sm btn-info" data-toogle="modal" data-tager="#modalcompra">
                     <i class="fas fa-shopping-basket"></i>
                 </a>
                 
             </td>
         </tr>
-        
         @endforeach
     </table>
+</div>
+
+<div class="modal-fade" id="modalcompra">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title">Detalhes da Compra</h5>
+            </div>
+    
+            <div class="modal-body">
+                
+             </div>
+            <div class="modarl-footer">
+                <button type="button" class="btn btn-sm btn-secondary">Fechar</button>
+                    
+            </div>
+        </div>
+    </div>
 </div>
 @endsection
--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -12,6 +12,15 @@
     <!-- Link Font Awesome-->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css">
 
+    <!-- Link  MODAL Jquery-->
+    <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+   
+    <!-- Link  MODAL BootStrapp-->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+
+
+
+
     @yield('scriptsjs')
 </head>
 
